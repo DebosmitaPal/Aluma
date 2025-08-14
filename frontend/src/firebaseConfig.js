966074673ce3374b60d4f233d17d99627cd0ba37
Aashutosh kumar bahrdwaj <ashutoshkumarbhardwaj7@gmail.com>
2025-08-14 14:51:07 +0530

    new one

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -5,13 +5,13 @@ import {  GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBPJPut3ukxqSqejyd4lsxWs4EEUv4Nhmw",
-  authDomain: "tryone-32076.firebaseapp.com",
-  projectId: "tryone-32076",
-  storageBucket: "tryone-32076.firebasestorage.app",
-  messagingSenderId: "327011966947",
-  appId: "1:327011966947:web:a4ed13c9b88e33fe2f4ad6",
-  measurementId: "G-V0E1BJ4FH4"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
